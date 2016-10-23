5ecd4ca68566c3af9e68a4f8da5dd294e03af2db
Gillian McCabe <gillianmccabe@outlook.com>
2016-10-23 02:11:59 +0100

    set up app.jsx

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,9 +1,10 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var Game = require('./components/Game');
 
 window.onload = function(){
   ReactDOM.render(
-    <h1> App Started </h1>,
+    <Game/>,
     document.getElementById('app')
   );
 }
